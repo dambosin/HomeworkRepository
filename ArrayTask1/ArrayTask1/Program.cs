8db332d2a87a482d15652edc62b714ab3b32bb83
Vlad <dambosin@gmail.com>
2021-04-08 19:24:30 +0300

    Solved array task 1

---
+++ ArrayTask1/ArrayTask1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArrayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for(int i = 0; i <= 9; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i <= 9; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
