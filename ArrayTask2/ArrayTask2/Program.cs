917c703dca4cd145ff143d0e77d7f637786e8a37
Vlad <dambosin@gmail.com>
2021-04-08 19:30:20 +0300

    Solved array task 2

---
+++ ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArrayTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"{arr[n - i - 1]} ");
+            }
+        }
+    }
+}
