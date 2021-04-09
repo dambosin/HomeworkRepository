e38fece68f442d47a6a85889ecbf244e0d48d691
Vlad <dambosin@gmail.com>
2021-04-09 19:45:46 +0300

    Solved string task 3

---
+++ String Task/StringTask3/StringTask3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someText = Console.ReadLine();
+            
+            for(int i = 0; i < someText.Length; i += 2)
+            {
+                someText = someText.Insert(i + 1, " ");
+            }
+
+            Console.WriteLine(someText);
+        }
+    }
+}
