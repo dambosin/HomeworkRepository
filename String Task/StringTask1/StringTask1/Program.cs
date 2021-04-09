aa3622ba41a89d6f8515ddfdc55241083f2a2bed
Vlad <dambosin@gmail.com>
2021-04-09 19:29:10 +0300

    Solved string task 1

---
+++ String Task/StringTask1/StringTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someText = Console.ReadLine();
+            Console.WriteLine($"You entered: {someText}");
+        }
+    }
+}
